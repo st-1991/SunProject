d33dc3f6f33a10419475fced54a3ad681f18ea29
suntao <suntao@weiche.cn>
2022-04-08 15:33:28 +0800

    更新代码

--- controllers/UniappController.go
+++ controllers/UniappController.go
@@ -1,15 +1,23 @@
 package controllers
 
 import (
-	"SunProject/libary/uniapp"
 	"github.com/gin-gonic/gin"
 )
 
 func UploadFile(c *gin.Context)  {
-	UniApp := uniapp.UniApp{}
-	token, err := UniApp.InitConfig().PreUploadFile("a.jpg")
-	if err != nil {
-		ApiResponse(c, &Response{Code: -1, Msg: "预加载失败"})
+	form, _ := c.MultipartForm()
+	files := form.File["files[]"]
+	filenames := make([]string, len(files))
+	for _, file := range files {
+		filename := file.Filename
+		if filename != "" {
+			filenames = append(filenames, file.Filename)
+		}
 	}
-	ApiResponse(c, &Response{Code: 200, Msg: "上传成功", Data: token})
+	//UniApp := uniapp.UniApp{}
+	//token, err := UniApp.InitConfig().PreUploadFile("a.jpg")
+	//if err != nil {
+	//	ApiResponse(c, &Response{Code: -1, Msg: "预加载失败"})
+	//}
+	ApiResponse(c, &Response{Code: 200, Msg: "上传成功", Data: filenames})
 }
